a2c7a8883f85736bd2f6f6554db82c44cd5eee47
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-09-09 11:12:42 -0300

    alter pg url

--- src/repositories/db.js
+++ src/repositories/db.js
@@ -5,8 +5,7 @@ async function connect() {
     return global.connection.connect();
   }
   const pool = new pg.Pool({
-    connectionString:
-      "postgres://ygmyaoej:7u5P9kLlwrxrVl3r-Q9EtjGAXK33eUyZ@chunee.db.elephantsql.com/ygmyaoej",
+    connectionString: process.env.PG_URL,
   });
   global.connection = pool;
   return pool.connect();
